18a17deec74e87060116f2881b07f733b7037cdd
Peter Todorov <petodorov@gmail.com>
2017-02-14 11:57:11 +0200

    .

--- LabExercises/LectureTests/LectureTests.cs
+++ LabExercises/LectureTests/LectureTests.cs
@@ -3,13 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
-namespace LectureTests
+class TriangleOfStars
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        int input = int.Parse(Console.ReadLine());
+
+        Console.Write("*",input);
+        Console.WriteLine();
+        for (int row = 0; row < input-2; row++)
         {
+            for (int col = 0; col <=row; col++)
+            {
+                Console.Write("*",row);
+            }
+            Console.WriteLine();
         }
+        Console.Write("*", input);
+
+
     }
 }
